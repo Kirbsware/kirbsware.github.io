d66c3e72a396349ec7160a002278335e25d13e89
Fezy <128897899+Kirbsware@users.noreply.github.com>
2023-09-02 14:49:11 +0100

    Delete grime directory

--- grime/typed.js
+++
@@ -1,9 +0,0 @@
-new Typed('#typed', {
-    strings: ['During your life, never stop dreaming.'],
-    typeSpeed: 120,
-    backSpeed: 120,
-    backDelay: 1000,
-    startDelay: 120,
-    loop: true,
-    showCursor: false
-});
