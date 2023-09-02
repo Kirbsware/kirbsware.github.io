d829650c233e1d42f76250701e79b87a15fe9a4c
Fezy <128897899+Kirbsware@users.noreply.github.com>
2023-09-02 02:59:53 +0100

    Delete typed.js

--- typed.js
+++
@@ -1,7 +0,0 @@
-new Typed('#typed', {
-    strings: ['Private Biolink','Click discord to have one','https://blades.gay :3','blades are gay?'],
-    typeSpeed : 35,
-    delaySpeed : 90,
-    loop: true,
-    showCursor: false
-});
