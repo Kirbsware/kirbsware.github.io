c6885ab0cd4c48e2dda776b45e778bc0121e7f7f
Fezy <128897899+Kirbsware@users.noreply.github.com>
2023-09-02 14:50:32 +0100

    Delete sosa directory

--- sosa/typed.js
+++
@@ -1,9 +0,0 @@
-new Typed('#typed', {
-    strings: ['@sosarust >_<'],
-    typeSpeed: 120,
-    backSpeed: 120,
-    backDelay: 1000,
-    startDelay: 120,
-    loop: true,
-    showCursor: false
-});
