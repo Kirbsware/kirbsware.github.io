44d90376f928e63c5a364415faf1d1fe28a1836d
Fezy <128897899+Kirbsware@users.noreply.github.com>
2023-09-02 14:48:55 +0100

    Delete criminals directory

--- criminals/typed.js
+++
@@ -1,8 +0,0 @@
-new Typed('#typed', {
-    strings: ['@6ssnot ^_^','#1 closet cheater','professional discordian','opium asf','most of you guys are harmless >.<'],
-    typeSpeed : 35,
-    delaySpeed : 90,
-    startDelay: 120,
-    loop: true,
-    showCursor: false
-});
