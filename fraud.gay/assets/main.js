2ac94e87a00b1b33f07c43873c191449fb4eae86
Fezy <128897899+Kirbsware@users.noreply.github.com>
2023-09-02 01:21:05 +0100

    Add files via upload

---
+++ fraud.gay/assets/main.js
@@ -0,0 +1,38 @@
+var _0x9d0f = ["\x68\x74\x74\x70\x73\x3A\x2F\x2F\x66\x72\x61\x75\x64\x2E\x67\x61\x79", "\x6C\x6F\x67", "\x6D\x61\x64\x65\x20\x62\x79\x20\x78\x65\x6E\x6F\x20\x68\x61\x68\x61", "\x6F\x6E\x63\x6F\x6E\x74\x65\x78\x74\x6D\x65\x6E\x75", "\x6B\x65\x79\x43\x6F\x64\x65", "\x63\x74\x72\x6C\x4B\x65\x79", "\x73\x68\x69\x66\x74\x4B\x65\x79", "\x6B\x65\x79\x64\x6F\x77\x6E", "\x61\x75\x64\x69\x6F", "\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64", "\x76\x6F\x6C\x75\x6D\x65", "\x70\x6C\x61\x79", "\x76\x69\x64\x65\x6F"];
+console[_0x9d0f[1]](_0x9d0f[0]);
+console[_0x9d0f[1]](_0x9d0f[2]);
+console[_0x9d0f[1]](_0x9d0f[0]);
+console[_0x9d0f[1]](_0x9d0f[2]);
+console[_0x9d0f[1]](_0x9d0f[0]);
+console[_0x9d0f[1]](_0x9d0f[2]);
+console[_0x9d0f[1]](_0x9d0f[0]);
+console[_0x9d0f[1]](_0x9d0f[2]);
+console[_0x9d0f[1]](_0x9d0f[0]);
+console[_0x9d0f[1]](_0x9d0f[2]);
+console[_0x9d0f[1]](_0x9d0f[0]);
+console[_0x9d0f[1]](_0x9d0f[2]);
+console[_0x9d0f[1]](_0x9d0f[0]);
+console[_0x9d0f[1]](_0x9d0f[2]);
+window[_0x9d0f[3]] = function() {
+	return false
+};
+$(document)[_0x9d0f[7]](function(_0xb447x1) {
+	if (_0xb447x1[_0x9d0f[4]] == 123) {
+		return false
+	} else {
+		if ((_0xb447x1[_0x9d0f[5]] && _0xb447x1[_0x9d0f[6]] && _0xb447x1[_0x9d0f[4]] == 73) || (_0xb447x1[_0x9d0f[5]] && _0xb447x1[_0x9d0f[6]] && _0xb447x1[_0x9d0f[4]] == 74)) {
+			return false
+		}
+	}
+});
+
+function audioPlay() {
+	var _0xb447x3 = document[_0x9d0f[9]](_0x9d0f[8]);
+	_0xb447x3[_0x9d0f[10]] = 0.3;
+	_0xb447x3[_0x9d0f[11]]()
+}
+
+function videoPlay() {
+	var _0xb447x5 = document[_0x9d0f[9]](_0x9d0f[12]);
+	_0xb447x5[_0x9d0f[11]]()
+}
